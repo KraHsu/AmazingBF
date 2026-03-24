c623222acb59e03e2a3070de0658d5f0e0cafd0e
KraHsu <charles040318@gmail.com>
2026-03-25 01:10:02 +0800

    feat: adapt to llvm18/22

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ mod interp;
 mod ir;
 mod runtime;
 
+#[cfg(all(feature = "llvm18", feature = "llvm22"))]
+compile_error!("features `llvm18` and `llvm22` cannot be enabled at the same time");
+
 fn main() {
     let config = match cli::parse_cli() {
         Ok(cfg) => cfg,
