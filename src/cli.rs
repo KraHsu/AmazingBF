2918fa1524e83fa4684af8f818dcf5957eb52b95
KraHsu <charles040318@gmail.com>
2026-03-25 17:49:13 +0800

    feat: different log level -q -vvv

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,7 @@
 use crate::driver::config::{DriverConfig, RunMode};
 
 use anyhow::Result;
-use clap::Parser;
+use clap::{ArgAction, Parser};
 use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
@@ -15,19 +15,29 @@ struct Args {
     #[arg(short, long, default_value = "a.out")]
     output: PathBuf,
 
-    /// Verbose mode
-    #[arg(short, long)]
-    verbose: bool,
+    /// Verbose level (-v, -vv, -vvv)
+    #[arg(short, long, action = ArgAction::Count)]
+    verbose: u8,
+
+    /// Quiet log
+    #[arg(short, long, action = ArgAction::SetTrue, group = "log_level")]
+    quiet: bool,
 
     /// Run mode
-    #[arg(short, long, value_enum, default_value_t = RunMode::Interpret)]
+    #[arg(short, long, value_enum, default_value_t = RunMode::Interpret, group = "log_level")]
     mode: RunMode,
 }
 
 #[derive(Debug, Clone)]
 pub struct AppConfig {
     pub driver_cfg: DriverConfig,
-    pub verbose: bool,
+
+    /// 0 -> quiet
+    /// 1 -> normal
+    /// 2 -> v
+    /// 3 -> vv
+    /// _ -> vvv
+    pub log_level: u8,
 }
 
 pub fn parse_cli() -> Result<AppConfig> {
@@ -41,12 +51,17 @@ pub fn parse_cli() -> Result<AppConfig> {
 
     let output = args.output;
 
+    if args.verbose > 3 {
+        eprintln!("verbose level must be at most 3");
+        std::process::exit(1);
+    }
+
     Ok(AppConfig {
         driver_cfg: DriverConfig {
             source,
             mode,
             output,
         },
-        verbose: args.verbose,
+        log_level: if args.quiet { 0 } else { args.verbose + 1 },
     })
 }
